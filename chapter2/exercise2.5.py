5b468f70b7a74f73effa7447b570952769b1afab
sbahlemoyo <157314718+sbahlemoyo@users.noreply.github.com>
2025-02-26 07:11:14 -0800

    chapter 4

--- chapter2/exercise2.5.py
+++ chapter2/exercise2.5.py
@@ -1,2 +1,2 @@
-famous_quote = "Uncle Ben oncesaid, 'with great power comes great responsibility.'"
+famous_quote = "Uncle Ben once said, 'with great power comes great responsibility.'"
 print(famous_quote)
