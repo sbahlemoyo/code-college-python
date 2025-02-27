451478b3ea1e63203c967a849ff341dc195a17ea
sbahlemoyo <157314718+sbahlemoyo@users.noreply.github.com>
2025-02-26 23:41:45 -0800

    more exercises from chapter3

---
+++ chapter3/exercise3.4.py
@@ -0,0 +1,5 @@
+guests = ["mom", "dad", "sister", "brother"]
+print(f'Hey {guests[0]}:\nI would like to invite you over for dinner.\nPlease RSVP so i can send you the deets.')
+print(f'Hey {guests[1]}:\nI would like to invite you over for dinner.\nPlease RSVP so i can send you the deets.')
+print(f'Hey {guests[2]}:\nI would like to invite you over for dinner.\nPlease RSVP so i can send you the deets.')
+print(f'Hey {guests[3]}:\nI would like to invite you over for dinner.\nPlease RSVP so i can send you the deets.')
---
+++ chapter3/exercise3.5.py
@@ -0,0 +1,11 @@
+guests = ["mom", "dad", "sister", "brother"]
+print(f'Hey {guests[0]}:\nI would like to invite you over for dinner.\nPlease RSVP so i can send you the deets.')
+print(f'Hey {guests[1]}:\nI would like to invite you over for dinner.\nPlease RSVP so i can send you the deets.')
+print(f'Hey {guests[2]}:\nI would like to invite you over for dinner.\nPlease RSVP so i can send you the deets.')
+print(f'Hey {guests[3]}:\nI would like to invite you over for dinner.\nPlease RSVP so i can send you the deets.')
+print(guests[2])
+guests[2] = "Aunty"
+print(f'Hey {guests[0]}:\nI would like to invite you over for dinner.\nPlease RSVP so i can send you the deets.')
+print(f'Hey {guests[1]}:\nI would like to invite you over for dinner.\nPlease RSVP so i can send you the deets.')
+print(f'Hey {guests[2]}:\nI would like to invite you over for dinner.\nPlease RSVP so i can send you the deets.')
+print(f'Hey {guests[3]}:\nI would like to invite you over for dinner.\nPlease RSVP so i can send you the deets.')
