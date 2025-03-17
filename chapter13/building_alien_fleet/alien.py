fd2a995651a587fd54b1ffd02dfd703d985e1161
sbahlemoyo <157314718+sbahlemoyo@users.noreply.github.com>
2025-03-18 01:54:24 +0200

    updated exercises

--- chapter13/building_alien_fleet/alien.py
+++ chapter13/building_alien_fleet/alien.py
@@ -10,7 +10,7 @@ class Alien(Sprite):
         self.screen = ai_game.screen
 
         # Load the alien image and set its rect attribute.
-        self.image = pygame.image.load('images/alien.bmp')
+        self.image = pygame.image.load('code-college-python/chapter13/building_alien_fleet/images/ship.bmp')
         self.rect = self.image.get_rect()
 
         # Start each new alien near the top left of the screen.
