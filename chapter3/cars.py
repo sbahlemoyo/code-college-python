24c564b85f66941ab27fcb08dd972f2b7466edd0
sbahlemoyo <157314718+sbahlemoyo@users.noreply.github.com>
2025-03-19 19:31:27 +0200

    updated exercises

---
+++ chapter3/cars.py
@@ -0,0 +1,29 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+#SORTING A LIST PERMANENTLY IN ALPHABETICAL ORDER
+cars.sort()
+print(cars)
+
+#SORTING A LIST PERMANENTLY IN REVERSE ALPHABETICAL ORDER
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort(reverse=True)
+print(cars)
+
+#TEMPORARILY SORTING A LIST IN ALPHABETICAL ORDER WITHOUT CHANGING THE ORIGINAL LIST ITSELF
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print("Here is the original list:")
+print(cars)
+print("\nHere is the sorted list:")
+print(sorted(cars))
+print("\nHere is the original list again:")
+print(cars)
+
+#PRINTING A LIST IN REVERSE
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+cars.reverse()
+print(cars)
+
+#THE LENGTH OF A LIST
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(len(cars))
---
+++ chapter3/motorcycles.py
@@ -0,0 +1,55 @@
+# motorcycles = ['honda', 'yamaha', 'suzuki']
+# print(motorcycles)
+
+#ACCESSING AN IITEM ON THE LIST
+
+# # motorcycles[0] = 'ducati'
+# # print(motorcycles)
+
+#APPENDING AN ITEM TO THE END OF A LIST
+
+# motorcycles.append('ducati')
+# print(motorcycles)
+
+motorcycles = []
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+
+#INSERTING AN ITEM IN A PARTICULAR PART/INDEX IN A LIST
+
+# print(motorcycles)
+# motorcycles.insert(1, 'ducati')
+# print(motorcycles)
+
+#PERMANENTLY REMOVING AN ITEM FROM A LIST
+
+# del motorcycles[0]
+# print(motorcycles)
+
+#USING POP METHOD TO REMOVE AN ITEM AT THE END OF A LIST
+motorcycles.pop()
+print(motorcycles)
+#REMOVING WITH POP BY INDEX
+motorcycles.pop(0)
+print(motorcycles)
+
+#REMOVING AN ITEM BY VALUE
+motorcycles.remove('ducati')
+print(motorcycles)
+
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f'\nA {too_expensive.title()} is too expensive for me.')
+
+#INDEX ERRORS (--OUT OF RANGE--)
+print(motorcycles[4])
+
+#PRINTING LAST ITEM IN THE LIST
+print(motorcycles[-1])
+
+
+
+
+
