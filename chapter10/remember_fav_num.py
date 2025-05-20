ab2cb964ba42dec5b258dc3b96edf9b24ad1d87e
sbahlemoyo <157314718+sbahlemoyo@users.noreply.github.com>
2025-05-21 01:14:20 +0200

    updates

--- chapter10/remember_fav_num.py
+++ chapter10/remember_fav_num.py
@@ -4,10 +4,13 @@ import json
 path = Path('favourite_num.json')
 if path.exists():
     contents = path.read_text()
-    fav_num = json.loads(contents)
-    print(f'Received. You\'re favourite number is {fav_num}')
+    user_info = json.loads(contents)
+    print(f'Hello {user_info["name"].title()}. Your favourite number is {user_info["fav_num"]}')
 
 else:
-    ask_fav_num = input('What is your fav num? ')
-    contents = json.dumps(ask_fav_num)
+    ask_fav_num = input('What is your fav num?')
+    ask_name = input('Enter your name:')
+    ask_last_name = input('Enter your last name:')
+    user_info = {'name': ask_name, 'surname': ask_last_name, 'fav_num': ask_fav_num}
+    contents = json.dumps(user_info)
     path.write_text(contents)
