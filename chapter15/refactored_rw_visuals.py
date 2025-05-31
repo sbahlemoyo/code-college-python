65b048aa1e214e58b681b81289b593ea517753b1
sbahlemoyo <157314718+sbahlemoyo@users.noreply.github.com>
2025-05-31 05:46:45 +0200

    updates

--- chapter15/refactored_rw_visuals.py
+++ chapter15/refactored_rw_visuals.py
@@ -4,13 +4,21 @@ from random_walk import RandomWalk
 #make a random walk and plot the points
 
 while True:
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
     
     plt.style.use('classic')
-    fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, c= rw.y_values, cmap=plt.cm.Reds , s=1)
-
+    fig, ax = plt.subplots(figsize=(15, 9), dpi=128)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
+    ax.set_aspect('equal')
+    # Emphasize the first and last points.
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
+    s=100)
+    # Remove the axes.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
     
     plt.show()
 
