2d3b5237e885e94b830ff4dcabbd66bfdf3680ba
sbahlemoyo <157314718+sbahlemoyo@users.noreply.github.com>
2025-08-05 09:48:21 +0200

    updates

--- chapter5/cars.py
+++ chapter5/cars.py
@@ -1,6 +1,6 @@
-cars = ['audi', 'bmw', 'subaru', 'toyota']
-for car in cars:
-    if car =='bmw':
-        print(car.upper())
-    else:
-        print(car.title())
+cars = ['audi', 'bmw', 'subaru', 'toyota']  # Create a list of car brands
+for car in cars:                            # Loop through each car in the list
+    if car == 'bmw':                        # Check if the current car is 'bmw'
+        print(car.upper())                  # Print 'bmw' in uppercase letters
+    else:                                   # If the car is not 'bmw'
+        print(car.title())                  # Print the car name with the first
--- chapter5/toppings.py
+++ chapter5/toppings.py
@@ -1,12 +1,12 @@
-available_toppings = ['mushrooms', 'olives', 'green peppers','pepperoni', 'pineapple', 'extra cheese']
+available_toppings = ['mushrooms', 'olives', 'green peppers','pepperoni', 'pineapple', 'extra cheese']  # List of toppings available in the shop
 
-requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']  # List of toppings requested by the customer
 
-for requested_topping in requested_toppings:
-    if requested_topping in available_toppings:
-        print(f"Adding {requested_topping}.")
+for requested_topping in requested_toppings:                        # Loop through each requested topping
+    if requested_topping in available_toppings:                     # Check if the topping is available
+        print(f"Adding {requested_topping}.")                       # Print message for adding the topping
     else:
-        print(f"Sorry, we don't have {requested_topping}.")
+        print(f"Sorry, we don't have {requested_topping}.")         # Print message if topping is not available
         
-print("\nFinished making your pizza!")
+print("\nFinished making your pizza!")                              # Print final message
 
