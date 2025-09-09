6d33d14900e6911f6aa51c25ae9a3e8c31d54cff
sbahlemoyo <157314718+sbahlemoyo@users.noreply.github.com>
2025-09-10 00:45:15 +0200

    updates

---
+++ chapter8/greet_users.py
@@ -0,0 +1,10 @@
+# Passing a list
+
+def greet_users(names):
+    """Printing a simple greeting to each user in the list."""
+    for name in names:
+        msg = f"Hello, {name.title()}!"
+        print(msg)
+
+usernames = ['hannah', 'ty', 'margot']
+greet_users(usernames)
