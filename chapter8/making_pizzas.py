824d3755d04b91eb3e3bfdde44873a22f9bf33fc
sbahlemoyo <157314718+sbahlemoyo@users.noreply.github.com>
2025-09-10 01:09:41 +0200

    updates

---
+++ chapter8/making_pizzas.py
@@ -0,0 +1,23 @@
+# Importing an entire module
+
+# import pizza
+
+# pizza.make_pizza(16, 'pepperoni')
+
+# # Importing specific functions
+# # from module_name import function_name
+# # from module_name import function_0, function_1, function_2
+
+# from pizza import make_pizza
+
+# Using as to give a function an alias
+
+# from pizza import make_pizza as mp
+
+# # Using as to give an entire module an alias
+
+# import pizza as p
+
+# # Importing all functions in a module
+
+# from pizza import *
--- chapter8/pizza.py
+++ chapter8/pizza.py
@@ -1,22 +1,22 @@
-# ####Passing an arbitrary number of arguments
+# # ####Passing an arbitrary number of arguments
 
-def make_pizza(*toppings):
-    """Print the list of toppings that have been requested"""
-    print(toppings)
+# def make_pizza(*toppings):
+#     """Print the list of toppings that have been requested"""
+#     print(toppings)
 
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
+# make_pizza('pepperoni')
+# make_pizza('mushrooms', 'green peppers', 'extra cheese')
 
-# Now we can replace the print() call with a loop that runs through the list of toppings and describes the pizza being ordered
+# # Now we can replace the print() call with a loop that runs through the list of toppings and describes the pizza being ordered
 
-def make_pizza(*toppings):
-    """Summarize the pizza we are about to make."""
-    print("\nMaking a pizza with the following toppings:")
-    for topping in toppings:
-        print(f"- {topping}")    
+# def make_pizza(*toppings):
+#     """Summarize the pizza we are about to make."""
+#     print("\nMaking a pizza with the following toppings:")
+#     for topping in toppings:
+#         print(f"- {topping}")    
 
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
+# make_pizza('pepperoni')
+# make_pizza('mushrooms', 'green peppers', 'extra cheese')
 
 # ###Mixing Positional and Arbitrary Arguments
 
